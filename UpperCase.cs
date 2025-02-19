137951d1635dd2657c3ed6e8632d30cc964fae45
Hameem Khair <hameemkhairinaz@gmail.com>
2025-02-19 22:00:19 +0600

    Create UpperCase.cs

---
+++ UpperCase.cs
@@ -0,0 +1,7 @@
+string userInput = "hamim khair";
+
+string inputUpper = userInput.ToUpper();
+
+Console.WriteLine($"User entered: {userInput}. Formatted String: {inputUpper}.");
+
+Console.ReadKey();
